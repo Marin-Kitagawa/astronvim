edf528d927f8cbe7162d098dff9b520471a05e1a
Marin Kitagawa <49131888+Marin-Kitagawa@users.noreply.github.com>
2025-03-24 21:24:16 +0530

    Added new plugins: `blink_cmp`, `markdown-preview`, `markview`, `mini`, `mini_icons`, `quicker`, `ufo`

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -1,41 +1,39 @@
----@type LazySpec
 return {
   "mikavilpas/yazi.nvim",
   event = "VeryLazy",
+  dependencies = { "folke/snacks.nvim", lazy = true },
   keys = {
     -- 👇 in this section, choose your own keymappings!
     {
       "<leader>-",
-      function()
-        require("yazi").yazi()
-      end,
-      desc = "Open the file manager",
+      mode = { "n", "v" },
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at the current file",
     },
     {
       -- Open in the current working directory
       "<leader>cw",
-      function()
-        require("yazi").yazi(nil, vim.fn.getcwd())
-      end,
-      desc = "Open the file manager in nvim's working directory" ,
+      "<cmd>Yazi cwd<cr>",
+      desc = "Open the file manager in nvim's working directory",
     },
     {
-      '<c-up>',
-      function()
-        -- NOTE: requires a version of yazi that includes
-        -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
-        require('yazi').toggle()
-      end,
+      "<c-up>",
+      "<cmd>Yazi toggle<cr>",
       desc = "Resume the last yazi session",
     },
   },
-  ---@type YaziConfig
+  ---@type YaziConfig | {}
   opts = {
     -- if you want to open yazi instead of netrw, see below for more info
     open_for_directories = false,
-
-    -- enable these if you are using the latest version of yazi
-    use_ya_for_events_reading = true,
-    use_yazi_client_id_flag = true,
+    keymaps = {
+      show_help = "<f1>",
+    },
   },
+  -- 👇 if you use `open_for_directories=true`, this is recommended
+  init = function()
+    -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+    -- vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+  end,
 }
