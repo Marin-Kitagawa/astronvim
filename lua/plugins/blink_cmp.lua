4994b5312a4b53675363974dbf22a17b0bab7551
Marin Kitagawa <49131888+Marin-Kitagawa@users.noreply.github.com>
2025-03-28 21:53:23 +0530

    Added some configuration for `blink-cmp` and `blink.pairs` and added the `conform` plugin

--- lua/plugins/blink_cmp.lua
+++ lua/plugins/blink_cmp.lua
@@ -1,50 +1,52 @@
 return {
-  'saghen/blink.cmp',
-  -- optional: provides snippets for the snippet source
-  dependencies = { 'rafamadriz/friendly-snippets' },
+  "saghen/blink.cmp",
+  event = { "InsertEnter", "CmdlineEnter" },
+  version = "v1.0.*",
+  build = "cargo build --release",
+  keys = {
+	  -- chartoggle
+	  {
+	    '<C-;>',
+	    function()
+	  	  require('blink.chartoggle').toggle_char_eol(';')
+	    end,
+	    mode = { 'n', 'v' },
+	    desc = 'Toggle ; at eol',
+	  },
+	  {
+	    ',',
+	    function()
+	  	  require('blink.chartoggle').toggle_char_eol(',')
+	    end,
+	    mode = { 'n', 'v' },
+	    desc = 'Toggle , at eol',
+	  },
 
-  -- use a release tag to download pre-built binaries
-  version = '*',
-  -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-  -- build = 'cargo build --release',
-  -- If you use nix, you can build from source using latest nightly rust with:
-  -- build = 'nix run .#build-plugin',
-
-  ---@module 'blink.cmp'
+	  -- -- tree
+	  -- { '<C-e>', '<cmd>BlinkTree reveal<cr>', desc = 'Reveal current file in tree' },
+	  -- { '<leader><E', '<cmd>BlinkTree toggle<cr>', desc = 'Reveal current file in tree' },
+	  -- { '<leader><leader>e', '<cmd>BlinkTree toggle-focus<cr>', desc = 'Toggle file tree focus' },
+  },
+  ---@module "blink.cmp"
   ---@type blink.cmp.Config
   opts = {
-    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-    -- 'super-tab' for mappings similar to vscode (tab to accept)
-    -- 'enter' for enter to accept
-    -- 'none' for no mappings
-    --
-    -- All presets have the following mappings:
-    -- C-space: Open menu or open docs if already open
-    -- C-n/C-p or Up/Down: Select next/previous item
-    -- C-e: Hide menu
-    -- C-k: Toggle signature help (if signature.enabled = true)
-    --
-    -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = { preset = 'default' },
-
-    appearance = {
-      -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- Adjusts spacing to ensure icons are aligned
-      nerd_font_variant = 'normal'
+    cmdline = {
+      enabled = true,
+      ---@diagnostic disable-next-line: assign-type-mismatch
+      sources = function()
+        local type = vim.fn.getcmdtype()
+        if type == "/" or type == "?" then
+          return { "buffer" }
+        end
+        if type == ":" or type == "@" then
+          return { "cmdline", "path" }
+        end
+        return {}
+      end,
+      completion = {
+        menu = { auto_show = true },
+        ghost_text = { enabled = false },
+      },
     },
-
-    -- Default list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, due to `opts_extend`
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
-
-    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-    -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-    -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-    --
-    -- See the fuzzy documentation for more information
-    fuzzy = { implementation = "prefer_rust_with_warning" }
-  },
-  opts_extend = { "sources.default" }
+  }
 }
