9f5f110246f1e1fb52072156330f1a823f8d3b65
Marin Kitagawa <49131888+Marin-Kitagawa@users.noreply.github.com>
2026-02-14 09:42:57 +0530

    fix: Minor syntax fix

--- lua/plugins/blink_cmp.lua
+++ lua/plugins/blink_cmp.lua
@@ -4,7 +4,7 @@ return {
   version = "*",
   dependencies = {
     "rafamadriz/friendly-snippets", -- Required for snippets
-  }
+  },
   build = "cargo build --release",
   keys = {
 	  -- chartoggle
