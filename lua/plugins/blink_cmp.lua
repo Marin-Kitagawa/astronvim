8fa38932e549b283f726e50b98bd5ebfa61d0283
Marin Kitagawa <49131888+Marin-Kitagawa@users.noreply.github.com>
2026-02-16 08:41:26 +0530

    fix: Removed `blink_cmp` `chartoggle`

--- lua/plugins/blink_cmp.lua
+++ lua/plugins/blink_cmp.lua
@@ -6,26 +6,7 @@ return {
     "rafamadriz/friendly-snippets", -- Required for snippets
   },
   build = "cargo build --release",
-  keys = {
-    -- chartoggle
-    {
-      "<C-;>",
-      function() require("blink.chartoggle").toggle_char_eol ";" end,
-      mode = { "n", "v" },
-      desc = "Toggle ; at eol",
-    },
-    {
-      ",",
-      function() require("blink.chartoggle").toggle_char_eol "," end,
-      mode = { "n", "v" },
-      desc = "Toggle , at eol",
-    },
 
-    -- -- tree
-    -- { '<C-e>', '<cmd>BlinkTree reveal<cr>', desc = 'Reveal current file in tree' },
-    -- { '<leader><E', '<cmd>BlinkTree toggle<cr>', desc = 'Reveal current file in tree' },
-    -- { '<leader><leader>e', '<cmd>BlinkTree toggle-focus<cr>', desc = 'Toggle file tree focus' },
-  },
   ---@module "blink.cmp"
   ---@type blink.cmp.Config
   opts = {
